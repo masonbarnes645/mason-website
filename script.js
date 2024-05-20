b6a08e2b285f4d1cfee5abb68785d0ed01443581
Mason Barnes <barnesmason980@gmail.com>
2024-05-20 11:26:24 -0700

    make button work on first click, change email link color

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const darkMode = document.getElementById("darkModeButton");
 const p = document.getElementById("info");
 const h1 = document.getElementById("hOne");
 const h2 = document.getElementById("hTwo");
-let isDarkMode = false;
+const link = document.getElementById("mail");
+let isDarkMode = true;
 
 
 
@@ -20,14 +21,16 @@ if(isDarkMode) {
         darkMode.textContent="Light";
         p.style.color="white";
         h1.style.color="white";
-        h2.style.color="white";  
+        h2.style.color="white"; 
+        mail.style.color="white"; 
         isDarkMode = false; }
 else {
         background.style.background="white";
         darkMode.textContent="Dark";
         p.style.color="black";
         h1.style.color="black";
-        h2.style.color="black";  
+        h2.style.color="black";
+        mail.style.color="black";  
         isDarkMode = true;
 }}
 
